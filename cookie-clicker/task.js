75daf9b655bce2f1c60c31fda2e4442a392ecaa7
Roman Podkorytov <romiraxp@mail.ru>
2025-04-21 15:48:23 +0500

    Correction

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -1,9 +1,14 @@
 const img = document.getElementById('cookie')
 const cnt = document.getElementById('clicker__counter')
+const wdt = img.width
 
 function changeImgSize() {
-    img.width +=30
+    if (img.width == wdt) {
+      img.width+=50
+    } else {
+      img.width -= 50
+    }
     cnt.textContent = Number(cnt.textContent) + 1
+    //    console.log(wdt)
 }
-  
   document.addEventListener('click', changeImgSize);
